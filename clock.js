dffde4e7aa01807ebb990b2944b854782841d612
goaldae <goaldae@naver.com>
2019-01-23 20:39:37 +0900

    display time

---
+++ clock.js
@@ -0,0 +1,15 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function init(){
+    getTime();
+}
+init();
